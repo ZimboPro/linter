3f992b218536fdfdfdcb8568d0dec2202fcb9cfb
Leslie <dejagerleslie@gmail.com>
2024-02-29 21:44:05 +0200

    Initial files packages and test files

---
+++ src/open_api_adapter.rs
@@ -0,0 +1,20 @@
+pub enum Vertex {
+    /// HCL Module
+    Module,
+    /// HCL Resource
+    Resource,
+    /// HCL Variable
+    Variable,
+    /// OpenAPI Path
+    Path,
+    /// OpenAPI Method
+    Method,
+    /// OpenAPI URI
+    Uri,
+    /// OpenAPI Tag
+    Tag,
+    /// OpenAPI ARN
+    Arn,
+    /// OpenAPI Template name
+    Template,
+}
